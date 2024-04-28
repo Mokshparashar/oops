df09b462dfa628876a460ff241d2d7f1a2477f18
Moksh parashar <parasharm239@gmail.com>
2024-04-28 06:34:26 -0700

    .

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-const body = document.querySelector("body");
-const button = document.querySelector("button");
+// const body = document.querySelector("body");
+// const button1 = document.querySelectorAll("button")[0];
+// const button2 = document.querySelectorAll("button")[1];
 
-button.addEventListener("click", () => {
-  localStorage.setItem("theme", "black");
-});
-const theme = localStorage.getItem("theme");
-body.style.backgroundColor = theme;
+// button1.addEventListener("click", () => {
+//   localStorage.setItem("theme", "black");
+//   const theme = localStorage.getItem("theme");
+//   body.style.backgroundColor = theme;
+// });
+// button2.addEventListener("click", () => {
+//   localStorage.setItem("theme", "white");
+//   const theme = localStorage.getItem("theme");
+//   body.style.backgroundColor = theme;
+// });
+
+// const theme = localStorage.getItem("theme");
+// body.style.backgroundColor = theme;
 
 class Name {
   constructor() {
     console.log("I am a constructor");
-  }
-
-  add(a, b) {
-    console.log(a + b);
   }
 
   subt(c, d) {
@@ -21,17 +26,26 @@ class Name {
   }
 }
 
-const duplicateName = new Name();
-
 class NewName extends Name {
-  constructor() {
-    console.log("I am the constructor of newname class");
-  }
   division(a, b) {
     console.log(a / b);
+    // super.add(a, b);
+  }
+
+  add(a, b) {
+    const c = a + b;
+    return c;
+  }
+
+  average(a, b) {
+    const val = this.add(a, b);
+
+    console.log(val / 2);
   }
 }
 
 const animal = new NewName();
 
-animal.division(5, 5);
+// animal.division(4, 5);
+
+console.log(animal instanceof Name);
